7ef3feb4d43690b54c2b3a32d0f2c90e171e56cb
jegatheeswaranvithurshan25-hash <jegatheeswaranvithurshan25@gmail.com>
2026-02-07 23:02:01 +0530

    initial

--- js/custom.js
+++ js/custom.js
@@ -1,56 +1,73 @@
+(function() {
+	'use strict';
 
-  (function ($) {
-  
-  "use strict";
+	var tinyslider = function() {
+		var el = document.querySelectorAll('.testimonial-slider');
 
-    // HERO SLIDE
-    $('.hero-slide').backstretch([
-      "images/slideshow/white-wall-living-room-have-sofa-decoration-3d-rendering.jpg", 
-      "images/slideshow/interior-wall-mockup-with-sofa-cabinet-living-room-with-empty-white-wall-background-3d-rendering.jpg",
-      "images/slideshow/wood-sideboard-living-room-interior-with-copy-space.jpg"
-    ],  {duration: 2000, fade: 750});
+		if (el.length > 0) {
+			var slider = tns({
+				container: '.testimonial-slider',
+				items: 1,
+				axis: "horizontal",
+				controlsContainer: "#testimonial-nav",
+				swipeAngle: false,
+				speed: 700,
+				nav: true,
+				controls: true,
+				autoplay: true,
+				autoplayHoverPause: true,
+				autoplayTimeout: 3500,
+				autoplayButtonOutput: false
+			});
+		}
+	};
+	tinyslider();
 
-    // REVIEWS CAROUSEL
-    $('.reviews-carousel').owlCarousel({
-    items:3,
-    loop:true,
-    dots: false,
-    nav: true,
-    autoplay: true,
-    margin:30,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:2
-        },
-        1000:{
-          items:3
-        }
-      }
-    })
-
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
-    var el = $(this).attr('href');
-    var elWrapped = $(el);
-    var header_height = $('.navbar').height();
-
-    scrollToDiv(elWrapped,header_height);
-    return false;
-
-    function scrollToDiv(element,navheight){
-      var offset = element.offset();
-      var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
-
-      $('body,html').animate({
-      scrollTop: totalScroll
-      }, 300);
-    }
-});
-    
-  })(window.jQuery);
+	
 
 
+	var sitePlusMinus = function() {
+
+		var value,
+    		quantity = document.getElementsByClassName('quantity-container');
+
+		function createBindings(quantityContainer) {
+	      var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
+	      var increase = quantityContainer.getElementsByClassName('increase')[0];
+	      var decrease = quantityContainer.getElementsByClassName('decrease')[0];
+	      increase.addEventListener('click', function (e) { increaseValue(e, quantityAmount); });
+	      decrease.addEventListener('click', function (e) { decreaseValue(e, quantityAmount); });
+	    }
+
+	    function init() {
+	        for (var i = 0; i < quantity.length; i++ ) {
+						createBindings(quantity[i]);
+	        }
+	    };
+
+	    function increaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        console.log(quantityAmount, quantityAmount.value);
+
+	        value = isNaN(value) ? 0 : value;
+	        value++;
+	        quantityAmount.value = value;
+	    }
+
+	    function decreaseValue(event, quantityAmount) {
+	        value = parseInt(quantityAmount.value, 10);
+
+	        value = isNaN(value) ? 0 : value;
+	        if (value > 0) value--;
+
+	        quantityAmount.value = value;
+	    }
+	    
+	    init();
+		
+	};
+	sitePlusMinus();
+
+
+})()
